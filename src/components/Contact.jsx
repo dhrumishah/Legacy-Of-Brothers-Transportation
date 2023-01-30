e3ffefb9ca5d505e19fe8cf324501fc865ce96bd
prateek <prateekjakhar123@gmail.com>
2023-01-30 20:18:09 +0530

    culture

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
               </label>
               <select
                 id="subject"
-                class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 placeholder="Subject*"
               >
                 <option selected>-- Select an option --</option>
