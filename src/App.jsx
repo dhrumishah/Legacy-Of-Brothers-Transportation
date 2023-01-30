8eb4757baa2ab58044e91f37f19ff7f902a8b53f
prateek <prateekjakhar123@gmail.com>
2023-01-30 18:17:22 +0530

    aboutus

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div>
       <BrowserRouter>
         <div className="relative">
-          {/* <Nav /> */}
+          <Nav />
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/aboutus" element={<AboutUs />} />
