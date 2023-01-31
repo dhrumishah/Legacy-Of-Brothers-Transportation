6ae469c16bc14daa4a3ae7369ef1b634db21ecc4
prateek <prateekjakhar123@gmail.com>
2023-02-01 01:22:54 +0530

    contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,47 +6,53 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <div className="relative">
+      <div className="relative flex flex-col">
         <img
           src={image_3}
           className="bg-no-repeat max-h-screen bg-cover bg-center z-[-1]"
         />
-        <img
+        {/* <img
           src={logo}
           className="absolute bg-no-repeat max-h-screen bg-cover bg-center  w-[18%] border-none top-4 left-7 sm:left-12 md:left-12 lg:left-16"
-        />
-        <div className="absolute flex flex-row-reverse text-[0.65rem] pt-6 gap-5 mr-6 justify-items-end text-white sm:gap-3 sm:text-[0.65rem] md:gap-3 md:text-[0.6rem] lg:gap-4 lg:text-[0.6rem] xl:text-[0.55rem]">
-          <div>Online Store</div>
-          <Link to="/culture">
-            <div>Culture</div>
-          </Link>
-          <div className="flex flex-col gap-3">
-            <div>Apply Here</div>
-            <div>Terms of Use</div>
-          </div>
-          <div className="flex flex-col gap-3">
-            <Link to="/contactus">
-              <div>Contact Us</div>
+        /> */}
+        <div className="absolute w-full top-0 right-0 flex flex-col gap-3 mr-9 sm:gap-0">
+          <div className="  flex flex-row-reverse text-[0.65rem] pt-6 gap-5  justify-items-end text-white sm:gap-3 sm:text-[0.7rem] md:gap-4 md:text-[0.65rem] lg:gap-4 lg:text-[0.75rem] xl:text-[0.6rem]">
+            <div>Online Store</div>
+            <Link to="/culture">
+              <div>Culture</div>
             </Link>
-            <div>Privacy Policy</div>
-          </div>
-          <div className="flex flex-col gap-3">
-            <Link to="/aboutus">
-              <div>About Us</div>
-            </Link>
-            <div>Social Media</div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div>Company Address</div>
-            <div className="text-[0.3rem] font-light sm:text-[0.4rem]">
-              <div>PO BOX 417</div>
-              <div>Des Plaines Ave, unit 491</div>
-              <div>Forest Park, Illinois 60130</div>
+            <div className="flex flex-col gap-3">
+              <div>Apply Here</div>
+              <div>Terms of Use</div>
             </div>
+            <div className="flex flex-col gap-3">
+              <Link to="/contactus">
+                <div>Contact Us</div>
+              </Link>
+              <div>Privacy Policy</div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Link to="/aboutus">
+                <div>About Us</div>
+              </Link>
+              <div>Social Media</div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <div>Company Address</div>
+              <div className="text-[0.3rem] font-light sm:text-[0.45rem] lg:text-[0.5rem] xl:text-[0.4rem]">
+                <div>PO BOX 417</div>
+                <div>Des Plaines Ave, unit 491</div>
+                <div>Forest Park, Illinois 60130</div>
+              </div>
+            </div>
+            <img
+              src={logo}
+              className="bg-no-repeat max-h-screen bg-cover bg-center ml-3 w-[15%] sm:w-[20%] border-none"
+            />
           </div>
-        </div>
-        <div className="absolute text-center text-[0.45rem] mt-5 text-white font-light sm:text-[0.6rem] sm:mt-6 md:text-[0.55rem] md:mt-5 ">
-          Copyright © Legacy of Brothers Transportation
+          <div className=" text-center text-[0.8rem]  text-white font-light sm:text-[0.7rem]  md:text-[0.75rem] xl:text-[0.65rem] ">
+            Copyright © Legacy of Brothers Transportation
+          </div>
         </div>
       </div>
     </>
