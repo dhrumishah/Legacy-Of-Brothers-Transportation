09ac05ca66415d57a3b5063205cfebb7268902b5
prateek <prateekjakhar123@gmail.com>
2023-02-07 16:33:41 +0530

    shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -7,7 +7,13 @@ import cap from "../assets/caps.png";
 import hat from "../assets/hats.png";
 
 const Shop = () => {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("red");
+
+  const redT = <img src={red} alt="" />;
+  const whiteT = <img src={white} alt="" />;
+  const yellowT = <img src={yellow} alt="" />;
+  const blackT = <img src={black} alt="" />;
+
   return (
     <>
       <div className="bg-[#F8F8F8]">
@@ -16,22 +22,25 @@ const Shop = () => {
         </h1>
         <div className="flex flex-col mx-auto pt-14 pb-10 text-3xl gap-10 text-center sm:flex-row sm:pb-72 sm:justify-center sm:gap-1 md:text-[0.8rem] lg:text-[0.9rem] xl:text-[0.8rem]">
           <div className="w-1/2 sm:w-1/5 mx-auto">
-            <img src={color ? color : "/src/assets/redT.png"} alt="" />
+            {color === "red" && redT}
+            {color === "white" && whiteT}
+            {color === "yellow" && yellowT}
+            {color === "black" && blackT}
             <div className="flex justify-center gap-5 sm:gap-2 py-2">
               <span
-                onClick={() => setColor("/src/assets/yellowT.png")}
+                onClick={() => setColor("yellow")}
                 class="w-7 h-7 sm:w-3 sm:h-3 bg-yellow-300 rounded-full cursor-pointer"
               ></span>
               <span
-                onClick={() => setColor("/src/assets/whiteT.png")}
+                onClick={() => setColor("white")}
                 class="w-7 h-7 sm:w-3 sm:h-3 bg-white rounded-full cursor-pointer"
               ></span>
               <span
-                onClick={() => setColor("/src/assets/blackT.png")}
+                onClick={() => setColor("black")}
                 class="w-7 h-7 sm:w-3 sm:h-3 bg-black rounded-full cursor-pointer"
               ></span>
               <span
-                onClick={() => setColor("/src/assets/redT.png")}
+                onClick={() => setColor("red")}
                 class="w-7 h-7 sm:w-3 sm:h-3 bg-red-500 rounded-full cursor-pointer"
               ></span>
             </div>
